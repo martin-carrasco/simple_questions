4f70a9690d45284c88da3fe0f5fecac4a2d0eac7
Martin Carrasco <martin@amigocloud.com>
2022-01-08 11:14:28 -0500

    (add) Base for django authentication

--- django/simple_questions/accounts/admin.py
+++ django/simple_questions/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import CustomUser
+from .forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+class CustomUserAdmin(BaseUserAdmin):
+    form = UserCreationForm
+    add_form = UserChangeForm
+
+    list_display = ('email', 'display_name', 'is_staff', 'username')
+    list_filter = ('is_staff',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'username')}),
+        ('Personal info', {'fields': ('display_name',)}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'display_name', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
